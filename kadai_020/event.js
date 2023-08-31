3d7a2d304348cf5935a3cd3cfdeaf6fe0d44940e
ichikawa-az <138674830+ichikawa-az@users.noreply.github.com>
2023-08-31 18:26:56 +0900

    kadai_020を提出

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+const change = document.getElementById('btn');
+
+
+  change.addEventListener('click', ()=>{
+    setTimeout(() => {
+    document.getElementById('text').innerHTML = 'ボタンがクリックされました';
+    }, 2000);
+  });
+
+  
+
